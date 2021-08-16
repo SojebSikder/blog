dcf1b73aedd50585b0d8e978a39c1dfea08a7f19
Sojeb Sikder <sojebsikder@gmail.com>
2021-08-16 16:12:27 +0600

    main

---
+++ app/Mail/PasswordMail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PasswordMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * $demo object instance
+     * 
+     * @var $demo
+     */
+    public $demo;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($demo)
+    {
+        $this->demo = $demo;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        // return $this->view('view.name');
+        return $this->from('sender@example.com')
+            ->view('mails.password')
+            ->text('mails.password_plain');
+    }
+}
